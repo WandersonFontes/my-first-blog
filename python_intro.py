e4f729d70bda354ad24a45aab47806559dc7a6fc
Lorena <lo.santana96@gmail.com>
2019-06-01 11:55:42 -0300

    My Django Girls app, frist commit

---
+++ python_intro.py
@@ -0,0 +1,13 @@
+# if 5 > 2:
+#     print("5 é maior que 2")
+# else:
+#     print("5 não é maior que 2")
+
+name = "Sonja"
+
+if name == "Ola":
+    print("Olá Ola!")
+elif name == "Sonja":
+    print("Olá Sonja!")
+else:
+    print("Olá estranho!")
